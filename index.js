d8bd62020785fb4a7771575e8d62117ba81d26c5
Tamle60991 <tamle.frontend@gmail.com>
2023-03-24 21:17:25 +0700

    add

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require("dotenv");
 const useRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 mongoose
@@ -15,6 +16,7 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", useRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Bkacend Server");
--- models/Product.js
+++ models/Product.js
@@ -2,16 +2,15 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array },
     size: { type: String },
     color: { type: String },
-    price: { type: String, required: true },
+    price: { type: Number, required: true },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
-mongoose.models = mongoose.model("Product", ProductSchema);
+
+module.exports = mongoose.model("Product", ProductSchema);
